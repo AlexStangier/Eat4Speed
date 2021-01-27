32d4ccbc85d90bd7a9066227379102c1b0abd038
AlexStangier <alex.stangier@gmail.com>
2021-01-27 23:00:07 +0100

    get for benutzer

--- src/main/java/de/eat4speed/GreetingResource.java
+++ src/main/java/de/eat4speed/GreetingResource.java
@@ -1,16 +1,24 @@
 package de.eat4speed;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import Database.Entites.Benutzer;
+import Database.Entites.Dtos.BenutzerDto;
+import Database.Manager.DatabaseUserService;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello-resteasy")
+
+@Path("/Benutzer")
 public class GreetingResource {
 
     @GET
+    @Path("get/{id}")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    public String getBenutzer(@PathParam("id") long id) {
+        Benutzer ben = Benutzer.findById(id);
+        return ("Name: "+ ben.getBenutzername());
     }
 }
+
