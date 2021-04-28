3b1b8365ea60c0a1a741ea2ed8f22b9420d9b012
Andreas S <singer.andreas@gmx.de>
2021-04-28 21:18:27 +0200

    Add routes for views

--- src/main/eat4speed_frontend/src/views/Kunde/routes.js
+++ src/main/eat4speed_frontend/src/views/Kunde/routes.js
@@ -1,6 +1,9 @@
 import KundeAnmeldung from '@/views/Kunde/KundeAnmeldung.vue'
 import Kunde from '@/views/Kunde/Kunde.vue'
-import KundeDish from '@/views/Kunde/KundeDish.vue'
+import KundeGericht from '@/views/Kunde/KundeGericht.vue'
+import KundeFavorites from '@/views/Kunde/KundeFavorites.vue'
+import KundeAuswahlseiteRestaurant from '@/views/Kunde/KundeAuswahlseiteRestaurant.vue'
+import KundeEinstellungen from '@/views/Kunde/KundeEinstellungen';
 
 export default [
     {
@@ -14,8 +17,23 @@ export default [
         component: Kunde
     },
     {
-        path: '/dish',
-        name: 'dish',
-        component: KundeDish
+        path: '/gericht',
+        name: 'Gericht',
+        component: KundeGericht
+    },
+    {
+        path: '/favorites',
+        name: 'Favorites',
+        component: KundeFavorites
+    },
+    {
+        path: '/restaurantAuswahl',
+        name: 'RestaurantAuswahl',
+        component: KundeAuswahlseiteRestaurant
+    },
+    {
+        path: '/einstellungen',
+        Name: 'Einstellungen',
+        component: KundeEinstellungen
     },
 ]
