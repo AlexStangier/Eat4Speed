a4e6c34e7022b2787b9dd5dc7bd8722128d7331c
Beni J <benij1401@gmail.com>
2021-04-30 17:37:59 +0200

    Schichtplan Ausnahmedialog hinzugefügt

--- src/main/eat4speed_frontend/src/views/Restaurant/RestaurantBestellungen.vue
+++ src/main/eat4speed_frontend/src/views/Restaurant/RestaurantBestellungen.vue
@@ -3,40 +3,62 @@
     <v-container fill-height fluid>
       <v-layout align-center justify-center>
         <v-flex md6 sm6 xs12>
-          <v-simple-table>
-            <template v-slot:default>
-              <thead>
-              <tr >
-                <th class="text-center">
-                  Bestellung
-                </th>
-                <th class="text-center">
-                  Offen
-                </th>
-                <th class="text-center">
-                  in Zubereitung
-                </th>
-                <th class="text-center">
-                  Abholbereit
-                </th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr
-                  v-for="item in bestellungen"
-                  :key="item.name"
+          <v-list>
+            <v-row class = "mr-5">
+            <h1>LOL</h1>
+              <v-spacer/>
+            <h1>LOL</h1>
+              <v-spacer/>
+            <h1>LOL</h1>
+            </v-row>
+            <v-list-item>
+              <v-btn
+                  class="mr-10"
+                  color="red"
+                  dark
+                  v-bind="attrs"
+                  v-on="on"
+                  @click="currentlyAdding = true"
               >
-                <td> <v-btn>{{ item.name }}</v-btn></td>
-                <td v-if="item.offen" bgcolor="green"></td>
-                <td v-else-if="!item.offen" bgcolor="red"></td>
-                <td v-if="item.inZubereitung" bgcolor="green"></td>
-                <td v-else-if="!item.inZubereitung" bgcolor="red"></td>
-                <td v-if="item.abholBereit" bgcolor="green"></td>
-                <td v-else-if="!item.abholBereit" bgcolor="red"></td>
-              </tr>
-              </tbody>
-            </template>
-          </v-simple-table>
+                Bestellung
+              </v-btn>
+              <v-slider
+                  v-model="fruits"
+                  :tick-labels="ticksLabels"
+                  :max="2"
+                  step="1"
+                  ticks="always"
+                  tick-size="10"
+              ></v-slider>
+            </v-list-item>
+            <v-list-item>
+              <v-btn
+                  class="mr-10"
+                  color="red"
+                  dark
+                  v-bind="attrs"
+                  v-on="on"
+                  @click="currentlyAdding = true"
+              >
+                Bestellung
+              </v-btn>
+              <v-slider
+                  v-model="fruits"
+                  :tick-labels="bestellstati"
+                  :max="2"
+                  step="1"
+                  ticks="always"
+                  tick-size="10"
+                  thumb-label
+                  thumb-size="100"
+              >
+                <template v-slot:thumb-label="{ value }">
+                  {{ bestellstati[value] }}
+                </template>
+              </v-slider>
+            </v-list-item>
+
+          </v-list>
         </v-flex>
       </v-layout>
     </v-container>
@@ -48,6 +70,7 @@ export default {
   name: "RestaurantBestellungen",
   data () {
     return {
+      bestellstati: ['Bereit', 'In Zubereitung', 'Abholbereit'],
       bestellungen: [
         {
           name: 'Bestellung 1',
