ab4a262ace5d7cfb41ff7c7c706c08c95bbdbb8d
Stephan Halter <stephan.halter@gmx.net>
2021-05-01 15:08:48 +0200

    changed Kunde

--- src/main/eat4speed_frontend/src/views/Kunde/KundeGericht.vue
+++ src/main/eat4speed_frontend/src/views/Kunde/KundeGericht.vue
@@ -1,58 +1,66 @@
-<template>
-  <v-main>
+<template><v-main>
+  <v-container>
+    <v-row>
+      <v-col cols="1"
+      >
+        <v-btn small to="/Kunde">Zurück</v-btn>
+      </v-col>
+      <v-col
+          order="2"
+          offset="8"
+      >
+        <v-btn small @click="addToCart">
+          Warenkorb
+          <v-icon>mdi-cart</v-icon>
+        </v-btn>
+      </v-col>
+      <v-col
+          order="3"
+      >
+        Preis
+      </v-col>
+    </v-row>
     <v-container>
-      <v-row>
-        <v-col cols="1"
-        >
-          <v-btn small to="kunde">Zurück</v-btn>
-        </v-col>
-      </v-row>
       <v-container>
-        <v-container>
-          <v-row>
-            <v-col
-                v-for="a in 2"
-                :key="a"
-            >
-              <v-img v-if="a === 1" src="https://ais.kochbar.de/vms/5ced0e371d90da128862f2c2/1200x1200/burger.jpg" alt="Bild von Essen" max-height="500" max-width="500"></v-img>
-              <v-content v-if="a === 2" class="text-left"> <!--Information-->
-                <v-row
-                    v-for="b in 10"
-                    :key="b"
+        <v-row>
+          <v-col
+              v-for="a in 2"
+              :key="a"
+          >
+            <v-img v-if="a === 1" :src="gerichtBild" alt="Bild von Essen" max-height="500" max-width="500"></v-img>
+            <v-content v-if="a === 2" align="left"> <!--Information-->
+              <v-row
+                  v-for="b in 10"
+                  :key="b"
+              >
+                <v-col
+                    v-for="c in 3"
+                    :key="c"
                 >
-                  <v-col
-                      v-for="c in 3"
-                      :key="c"
-                  >
-                    <v-content v-if="b === 1 & c === 1">
-                      <h1>Burger</h1>
-                    </v-content>
-                    <v-content v-if="b === 1 & c === 3" class="text-right">
-                      <v-btn small="true" right="right">
-                        <v-icon>mdi-heart</v-icon>
-                      </v-btn>
-                    </v-content>
-                    <v-content v-if="b === 2 & c === 1">
-                      <h3>Bob's Burger</h3>
-                    </v-content>
-                    <v-content v-if="b === 10 & c === 1">
-                      Anzahl
-                    </v-content>
-                    <v-content v-if="b === 10 & c === 2">
-                      Preis
-                    </v-content>
-                    <v-content v-if="b === 10 & c === 3">
-                      <v-btn>Bestellen</v-btn>
-                    </v-content>
-                  </v-col>
-                </v-row>
-              </v-content>
-            </v-col>
-          </v-row>
-          <br>
-          <v-textarea outlined readonly no-resize rows="8" value="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-
-Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,"></v-textarea>
+                  <v-content v-if="b === 1 & c === 1">
+                    <v-text-field readonly v-model="gerichtName"></v-text-field>
+                  </v-content>
+                  <v-content v-if="b === 2 & c === 1">
+                    <v-text-field readonly v-model="restaurantName"></v-text-field>
+                  </v-content>
+                  <v-content v-if="b === 10 & c === 1">
+                    <v-text-field label="Anzahl" v-model="gerichtAnzahl" type="number">
+                    </v-text-field>
+                  </v-content>
+                  <v-content v-if="b === 10 & c === 2">
+                    Preis
+                    <v-text-field readonly v-model="gerichtPreis"></v-text-field>
+                  </v-content>
+                  <v-content v-if="b === 10 & c === 3">
+                    <v-btn>Bestellen</v-btn>
+                  </v-content>
+                </v-col>
+              </v-row>
+            </v-content>
+          </v-col>
+        </v-row>
+        <br>
+        <v-textarea outlined readonly no-resize rows="8" v-model="gerichtBeschreibung"></v-textarea>
         </v-container>
       </v-container>
     </v-container>
@@ -60,8 +68,88 @@ Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie co
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: "KundeGericht"
+  name: "KundeDish",
+  mounted(){
+    this.gericht_ID = this.$store.getters.gericht_ID;
+    this.loadGericht();
+  },
+  methods: {
+    async loadGericht() {
+      const ResponseGerichte = await axios.get("Gericht/getGerichtDataByGericht_ID/" + this.gericht_ID);
+
+      console.log(ResponseGerichte);
+
+      for (let i = 0; i < ResponseGerichte.data.length; i++) {
+        let gerichtData = ResponseGerichte.data[i];
+
+        this.gerichtName = gerichtData[1];
+        this.gerichtBeschreibung = gerichtData[2];
+        this.gerichtPreis = gerichtData[3] + "€";
+        this.gerichtVerfuegbar = gerichtData[4];
+        this.restaurant_ID = gerichtData[5];
+        this.restaurantName = gerichtData[6];
+        this.restaurantMindestbestellwert = gerichtData[7];
+      }
+
+      for (let i = 0; i < ResponseGerichte.data.length; i++)
+      {
+        const config = { responseType:"arraybuffer" };
+        const responsePicture = await axios.get("/GerichtBilder/getBild/"+this.gericht_ID,config);
+
+        console.log(responsePicture);
+
+        if(responsePicture.status !== 204)
+        {
+          console.log("received Picture")
+          console.log(responsePicture.data);
+
+          let pictureBlob = new Blob([responsePicture.data], { type : responsePicture.headers["content-type"]})
+
+          let imageURL = URL.createObjectURL(pictureBlob);
+          console.log(imageURL);
+
+          this.gerichtBild = imageURL;
+        }
+        else
+        {
+          this.gerichtBild = "";
+        }
+
+      }
+      this.version++;
+
+    },
+    addToCart() {
+
+      let cartGericht = {
+        gericht_ID: this.gericht_ID,
+        name: this.gerichtName,
+        thumbnail: this.gerichtBild,
+        quantity: this.gerichtAnzahl
+      }
+
+      this.$store.commit("addToCartGerichte", cartGericht);
+      console.log("Current Cart: "+this.$store.getters.getCartGerichte[0]);
+    }
+  },
+  data: () => ({
+    gerichtName: "",
+    gerichtBeschreibung: "",
+    gerichtBild: "",
+    gerichtPreis: "",
+    gerichtVerfuegbar: "",
+    gerichtAnzahl: 0,
+    cartGerichte: "",
+    restaurant_ID: "",
+    restaurantName: "",
+    restaurantMindestbestellwert: "",
+    gericht_ID: "",
+    version: 0,
+
+  }),
 }
 </script>
 
