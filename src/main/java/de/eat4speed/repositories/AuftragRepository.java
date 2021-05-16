ec04ce639a11beb63c8adca0816d33654b26ed1a
alexstangier <alex.stangier@gmail.com>
2021-05-16 17:28:32 +0200

    paypal dtos

--- src/main/java/de/eat4speed/repositories/AuftragRepository.java
+++ src/main/java/de/eat4speed/repositories/AuftragRepository.java
@@ -2,13 +2,19 @@ package de.eat4speed.repositories;
 
 import de.eat4speed.entities.Auftrag;
 
+import de.eat4speed.entities.Benutzer;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
 @ApplicationScoped
 public class AuftragRepository implements PanacheRepository<Auftrag> {
+
+    @Inject
+    EntityManager entityManager;
 
     @Transactional
     public void addAuftrag(Auftrag auftrag) {
@@ -24,5 +30,10 @@ public class AuftragRepository implements PanacheRepository<Auftrag> {
     public void updateAuftragStatus(int id, String status)
     {
         update("status = ?1 where id = ?2",status,id);
+    }
+
+    @Transactional
+    public Auftrag getAuftragByID(int id) {
+        return find("Auftrags_ID", id).firstResult();
     }
 }
---
+++ src/main/java/de/eat4speed/services/PaypalService.java
@@ -0,0 +1,89 @@
+package de.eat4speed.services;
+
+import com.paypal.http.HttpResponse;
+import com.paypal.http.exceptions.HttpException;
+import com.paypal.orders.*;
+import de.eat4speed.dto.PaypalDto;
+import de.eat4speed.entities.Auftrag;
+import de.eat4speed.entities.Bestellung;
+import de.eat4speed.entities.Rechnung;
+import de.eat4speed.paypal.Credentials;
+import de.eat4speed.repositories.AuftragRepository;
+import de.eat4speed.repositories.BestellungRepository;
+import de.eat4speed.repositories.RechnungRepository;
+import de.eat4speed.services.interfaces.IPaypalService;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@ApplicationScoped
+public class PaypalService implements IPaypalService {
+
+    private AuftragRepository _auftragRepository;
+    private RechnungRepository _rechnungRepository;
+    private BestellungRepository _bestellungRepository;
+
+    @Inject
+    public PaypalService(AuftragRepository auftragRepository,
+                         RechnungRepository rechnungRepository,
+                         BestellungRepository bestellungRepository) {
+        _auftragRepository = auftragRepository;
+        _rechnungRepository = rechnungRepository;
+        _bestellungRepository = bestellungRepository;
+    }
+
+    @Override
+    public PaypalDto checkOutViaPaypal(long auftragId) {
+
+        //retrieve auftrag from database
+        Auftrag auftrag = _auftragRepository.getAuftragByID((int) auftragId);
+        Bestellung bestellung = null;
+        Order order = null;
+        PaypalDto ppDto = null;
+        List<String> resultLinks = new ArrayList<>();
+
+        if (auftrag != null) {
+            bestellung = _bestellungRepository.getBestellungByID((int) auftrag.getAuftrags_ID());
+            if (bestellung != null) {
+                Rechnung rechnung = _rechnungRepository.getRechnungByID(bestellung.getRechnung());
+
+                OrderRequest orderRequest = new OrderRequest();
+                orderRequest.checkoutPaymentIntent("CAPTURE");
+                List<PurchaseUnitRequest> purchaseUnits = new ArrayList<>();
+                purchaseUnits.add(new PurchaseUnitRequest().amountWithBreakdown(new AmountWithBreakdown()
+                        .currencyCode("EUR").value(String.valueOf(rechnung.getBetrag()))));
+                orderRequest.purchaseUnits(purchaseUnits);
+                OrdersCreateRequest request = new OrdersCreateRequest().requestBody(orderRequest);
+
+                try {
+                    HttpResponse<Order> response = Credentials.client.execute(request);
+                    order = response.result();
+                    ppDto.setOrderId(order.id());
+                    order.links().forEach(link -> resultLinks.add(link.rel() + " => " + link.method() + ":" + link.href()));
+                    order.links().forEach(link -> System.out.println(link.rel() + " => " + link.method() + ":" + link.href()));
+                    ppDto.setSelfLink(resultLinks.get(0));
+                    ppDto.setApproveLink(resultLinks.get(1));
+                    ppDto.setUpdateLink(resultLinks.get(2));
+                    ppDto.setCaptureLink(resultLinks.get(3));
+                } catch (IOException ioe) {
+                    if (ioe instanceof HttpException) {
+                        // Something went wrong server-side
+                        HttpException he = (HttpException) ioe;
+                        System.out.println(he.getMessage());
+                        he.headers().forEach(x -> System.out.println(x + " :" + he.headers().header(x)));
+                    } else {
+                        System.out.println("Client fucked up payment!!1111!!!111!");
+                    }
+                }
+            } else {
+                return new PaypalDto();
+            }
+
+        }
+        return ppDto;
+    }
+}
