90ec114c5836fee38fa0b2b9ebf539e1143527a2
root-as <singerartur+github2@gmail.com>
2021-06-07 21:35:25 +0200

    after v2 load

--- eat4speed-frontend/src/views/Restaurant/RestaurantBestellungen.vue
+++ eat4speed-frontend/src/views/Restaurant/RestaurantBestellungen.vue
@@ -1,126 +1,35 @@
 <template>
   <v-main>
-
-    <v-app-bar color="blue"  dark>
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-      <v-toolbar-title>Bestellungen</v-toolbar-title>
-    </v-app-bar>
-    <v-navigation-drawer
-        v-model="drawer"
-        absolute
-        temporary
-    >
-      <v-list
-          nav
-          dense
-      >
-        <v-list-item-group
-            v-model="group"
-            active-class="deep-purple--text text--accent-4"
-        >
-          <router-link  to="/restaurant/controlpanel">
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>ControlPanel</v-list-item-title>
-          </v-list-item>
-          </router-link>
-          <router-link  to="/restaurant/speisekarteGerichte"><v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-silverware</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Speisekarte bearbeiten</v-list-item-title>
-          </v-list-item>
-          </router-link>
-          <router-link  to="/restaurant/bestellungen"><v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-view-headline</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Offene Bestellungen</v-list-item-title>
-          </v-list-item>
-          </router-link>
-          <router-link to="/restaurant/schichtplan">
-            <v-list-item>
-              <v-list-item-icon>
-                <v-icon>mdi-watch</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>Schichtplan</v-list-item-title>
-            </v-list-item>
-          </router-link>
-          <router-link  to="/restaurant/stammdaten"><v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Stammdaten</v-list-item-title>
-          </v-list-item>
-          </router-link>
-          <router-link  to="/restaurant/umsatzstatistik">
-            <v-list-item>
-              <v-list-item-icon>
-                <v-icon>mdi-margin</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>Umsatzstatistik</v-list-item-title>
-            </v-list-item>
-          </router-link>
-
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-
     <v-container fill-height fluid>
       <v-layout align-center justify-center>
         <v-flex md6 sm6 xs12>
-          <h1 class="mb-5">Offene Bestellungen (verschwinden wenn Kunde sie erhält)</h1>
+          <h1 class="mb-5">Offene Bestellungen</h1>
           <v-list>
-
-            <v-list-item class="mb-12">
+            <v-list-item class="mb-12" v-for="item in eingegangeneBestellungen" v-bind:key="item.id">
               <v-card class="mr-10" color="red" dark>
-              <v-col >
-                <v-card-title>Bestellung 1 (Name des kunden?)</v-card-title>
-                <v-card-text>- Pizza Mozarella</v-card-text>
-                <v-card-text>- Cola groß</v-card-text>
-
-              </v-col>
-              </v-card>
-              <v-slider
-                  :tick-labels="bestellstati"
-                  :max="2"
-                  step="1"
-                  ticks="always"
-                  tick-size="0"
-                  thumb-size="100"
-              >
-                <template v-slot:thumb-color="{ value }">
-                  {{ bestellstatifarben[value] }}
-                </template>
-                <template v-slot:thumb-label="{ value }">
-                  {{ bestellstati[value] }}
-                </template>
-              </v-slider>
-            </v-list-item>
-            <v-list-item class="mb-12">
-              <v-card class="mr-10" color="red" dark>
-                <v-col>
-                  <v-card-title>Bestellung 2 (Name des kunden?)</v-card-title>
-                  <v-card-text>- Calzone</v-card-text>
-                  <v-card-text>- Cola groß</v-card-text>
-
+                <v-col >
+                  <v-card-title>Bestellung {{ item.id }} - {{ item.name }}</v-card-title>
+                  <v-card-text>- {{ item.artikel }}</v-card-text>
+                  {{ item.price }} €
                 </v-col>
               </v-card>
               <v-slider
+                  :value="statusNummer"
                   :tick-labels="bestellstati"
-                  :max="2"
+                  :max="3"
                   step="1"
                   ticks="always"
                   tick-size="0"
                   thumb-size="100"
+                  @change="changeAuftragStatus(item.id, item.status)"
+
+
               >
                 <template v-slot:thumb-color="{ value }">
                   {{ bestellstatifarben[value] }}
                 </template>
                 <template v-slot:thumb-label="{ value }">
-                  {{ bestellstati[value] }}
+                  {{ item.status = bestellstati[value] }}
                 </template>
               </v-slider>
             </v-list-item>
@@ -133,30 +42,76 @@
 </template>
 
 <script>
+import axios from "axios";
+
+
 export default {
   name: "RestaurantBestellungen",
+  mounted() {
+    this.loadBestellungen();
+  },
+  methods: {
+    async loadBestellungen() {
+
+      const ResponseBestellungen = await axios.get("Benutzer/getRestaurantBestellungen/" + this.$store.getters.getLoginData.auth.username);
+
+      let anzahl = ResponseBestellungen.data.length.toString();
+
+      let item;
+      for(let i = 0; i < anzahl; i++) { // outer loop
+        if((ResponseBestellungen.data[i][2]) == 'bezahlt'){
+          this.statusNummer = 1;
+        }
+        if((ResponseBestellungen.data[i][2]) == 'bearbeitung'){
+          this.statusNummer = 2;
+        }
+        if((ResponseBestellungen.data[i][2]) == 'abholbereit'){
+          this.statusNummer = 3;
+        }
+
+        item = {id: (ResponseBestellungen.data[i][0]), name: (ResponseBestellungen.data[i][1]),
+          price: parseFloat(ResponseBestellungen.data[i][3]).toFixed(2).toString(),
+          artikel: (ResponseBestellungen.data[i][4]), status: this.statusNummer}
+
+        this.eingegangeneBestellungen.push( item )
+
+      }
+
+    },
+    async changeAuftragStatus(bestellID, zustand){
+
+      if(zustand == 'stornieren'){
+        zustand = 'storniert';
+      }
+      if(zustand == 'Bereit'){
+        zustand = 'bezahlt';
+      }
+      if(zustand == 'In Zubereitung'){
+        zustand = 'bearbeitung';
+      }
+      if(zustand == 'Abholbereit'){
+        zustand = 'abholbereit';
+      }
+
+      let auftrag = {
+        status: zustand,
+        auftrags_ID: bestellID
+      }
+
+      await axios.put("/Auftrag/updateAuftragStatusRestaurant", auftrag);
+
+      if(zustand == 'storniert'){
+        window.location.reload();
+      }
+
+    }
+  },
   data () {
     return {
-      drawer: false,
-      group: null,
-      switch1: true,
-      bestellstati: ['Bereit', 'In Zubereitung', 'Abholbereit'],
+      bestellstati: ['stornieren', 'Bereit', 'In Zubereitung', 'Abholbereit'],
       bestellstatifarben: ['red', 'yellow', 'green'],
-      bestellungen: [
-        {
-          name: 'Bestellung 1',
-          offen: true,
-          inZubereitung: true,
-          abholBereit: false,
-        },
-        {
-          name: 'Bestellung 2',
-          offen:  true,
-          inZubereitung: false,
-          abholBereit: false,
-        },
-
-      ],
+      eingegangeneBestellungen: [],
+      statusNummer: '',
     }
   },
 }
