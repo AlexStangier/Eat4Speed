c640ba05418c4fd795cd684f367d18aef12a3b62
Stephan Halter <stephan.halter@gmx.net>
2021-06-09 19:33:48 +0200

    edited entities and added methods for Benachrichtigung_Fahrer

--- eat4speed-backend/src/main/java/de/eat4speed/controllers/Benachrichtigung_FahrerController.java
+++ eat4speed-backend/src/main/java/de/eat4speed/controllers/Benachrichtigung_FahrerController.java
@@ -1,26 +1,37 @@
 package de.eat4speed.controllers;
 
 
+import de.eat4speed.entities.Gericht;
 import de.eat4speed.repositories.Benachrichtigung_FahrerRepository;
 import de.eat4speed.repositories.FahrzeugRepository;
+import de.eat4speed.services.interfaces.IBenachrichtigung_FahrerService;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/Benachrichtigung_Fahrer")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class Benachrichtigung_FahrerController {
 
 
     @Inject
-    Benachrichtigung_FahrerRepository benachrichtigung_FahrerRepository;
+    IBenachrichtigung_FahrerService benachrichtigung_fahrerService;
+
+    @PUT
+    @Path("/markAsGelesen/{benachrichtigungs_ID}")
+    public void markAsGelesen(@PathParam("benachrichtigungs_ID") int benachrichtigungs_ID)
+    {
+        benachrichtigung_fahrerService.markAsGelesen(benachrichtigungs_ID);
+    }
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String get(){
-        return benachrichtigung_FahrerRepository.listAll().toString();
+    @Path("/getAllBenachrichtigungFahrerUngelesen/{fahrernummer}")
+    public List getAllBenachrichtigung_Fahrer_ungelesen(@PathParam("fahrernummer") int fahrernummer)
+    {
+        return benachrichtigung_fahrerService.getAllBenachrichtigung_Fahrer_ungelesen(fahrernummer);
     }
 
 }
--- eat4speed-backend/src/main/java/de/eat4speed/entities/Bestellung.java
+++ eat4speed-backend/src/main/java/de/eat4speed/entities/Bestellung.java
@@ -20,6 +20,8 @@ public class Bestellung extends PanacheEntityBase implements Serializable {
     private Timestamp timestamp;
     private int rechnung;
     private String Gericht_IDs;
+    private String status;
+    private int restaurant_ID;
     public Bestellung() {
     }
     public Bestellung(int auftrags_ID, Timestamp timestamp, int rechnung) {
@@ -32,29 +34,8 @@ public class Bestellung extends PanacheEntityBase implements Serializable {
         return bestell_ID;
     }
 
-    public void setBestell_ID(int bestellId) {
-        this.bestell_ID = bestellId;
-    }
-
-    public Timestamp getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(Timestamp timestamp) {
-        this.timestamp = timestamp;
-    }
-
-
-    public Integer getRechnung() {
-        return rechnung;
-    }
-
-    public void setRechnung(Integer rechnung) {
-        this.rechnung = rechnung;
-    }
-
-    public void setRechnung(int rechnung) {
-        this.rechnung = rechnung;
+    public void setBestell_ID(int bestell_ID) {
+        this.bestell_ID = bestell_ID;
     }
 
     public int getAuftrags_ID() {
@@ -65,6 +46,22 @@ public class Bestellung extends PanacheEntityBase implements Serializable {
         this.auftrags_ID = auftrags_ID;
     }
 
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getRechnung() {
+        return rechnung;
+    }
+
+    public void setRechnung(int rechnung) {
+        this.rechnung = rechnung;
+    }
+
     public String getGericht_IDs() {
         return Gericht_IDs;
     }
@@ -73,17 +70,33 @@ public class Bestellung extends PanacheEntityBase implements Serializable {
         Gericht_IDs = gericht_IDs;
     }
 
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getRestaurant_ID() {
+        return restaurant_ID;
+    }
+
+    public void setRestaurant_ID(int restaurant_ID) {
+        this.restaurant_ID = restaurant_ID;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Bestellung that = (Bestellung) o;
-        return bestell_ID == that.bestell_ID && auftrags_ID == that.auftrags_ID && rechnung == that.rechnung && Objects.equals(timestamp, that.timestamp);
+        return bestell_ID == that.bestell_ID && auftrags_ID == that.auftrags_ID && rechnung == that.rechnung && restaurant_ID == that.restaurant_ID && Objects.equals(timestamp, that.timestamp) && Objects.equals(Gericht_IDs, that.Gericht_IDs) && Objects.equals(status, that.status);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(bestell_ID, auftrags_ID, timestamp, rechnung);
+        return Objects.hash(bestell_ID, auftrags_ID, timestamp, rechnung, Gericht_IDs, status, restaurant_ID);
     }
 
     @Override
@@ -93,6 +106,9 @@ public class Bestellung extends PanacheEntityBase implements Serializable {
                 ", auftrags_ID=" + auftrags_ID +
                 ", timestamp=" + timestamp +
                 ", rechnung=" + rechnung +
+                ", Gericht_IDs='" + Gericht_IDs + '\'' +
+                ", status='" + status + '\'' +
+                ", restaurant_ID=" + restaurant_ID +
                 '}';
     }
 }
