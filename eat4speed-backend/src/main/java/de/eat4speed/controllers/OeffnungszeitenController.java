5b7ca4515d2d23dc29624830887dac64c38243bf
Markus Roesch <>
2021-06-18 18:36:50 +0200

    Implement TST009

--- eat4speed-backend/src/main/java/de/eat4speed/controllers/OeffnungszeitenController.java
+++ eat4speed-backend/src/main/java/de/eat4speed/controllers/OeffnungszeitenController.java
@@ -1,46 +1,39 @@
 package de.eat4speed.controllers;
 
 
-import de.eat4speed.entities.Gericht;
 import de.eat4speed.entities.Oeffnungszeiten;
-import de.eat4speed.repositories.OeffnungszeitenRepository;
-import de.eat4speed.services.OeffnungszeitenService;
 import de.eat4speed.services.interfaces.IOeffnungszeitenService;
 
 import javax.inject.Inject;
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
-import java.sql.Timestamp;
 import java.util.List;
 
 @Path("/Oeffnungszeiten")
 public class OeffnungszeitenController {
 
-
     @Inject
     IOeffnungszeitenService oeffnungszeitenService;
-
 
     @POST
     @Path("setArbeitstag")
     public Response add(Oeffnungszeiten zeit){
-        oeffnungszeitenService.setArbeitstag(zeit);
-
-        return Response.status(Response.Status.CREATED).entity(zeit).build();
-    };
+        return oeffnungszeitenService.setArbeitstag(zeit);
+    }
 
     @PUT
     @Path("updateArbeitstag")
-    public Response updateArbeitstag(Oeffnungszeiten zeit)
-    {
+    public Response updateArbeitstag(Oeffnungszeiten zeit) {
         return oeffnungszeitenService.updateArbeitstag(zeit);
     }
 
     @GET
     @Path("/getAllZeiten/{id}")
-    public List getAllZeiten(@PathParam("id") int restaurant_id){
-
+    public List getAllZeiten(@PathParam("id") int restaurant_id) {
         return oeffnungszeitenService.getAllZeiten(restaurant_id);
     }
 
