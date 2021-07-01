e0a508ae39e9f78e39e3c0586ee3900b4747b857
Sönke G <sguenthe@stud.hs-offenburg.de>
2021-07-01 23:27:39 +0200

    http -> https

--- eat4speed-backend/src/main/java/de/eat4speed/FahrerAuswahl_FahrtenVergabe/Algo_FahrerAuswahl.java
+++ eat4speed-backend/src/main/java/de/eat4speed/FahrerAuswahl_FahrtenVergabe/Algo_FahrerAuswahl.java
@@ -125,7 +125,7 @@ public class Algo_FahrerAuswahl {
     {
         URL url;
         try {
-            url = new URL("http:/eat4speed.xyz/#/FahrerAuswahl/" + startPunktID);
+            url = new URL("https://eat4speed.xyz/FahrerAuswahl/" + startPunktID);
             HttpURLConnection http = (HttpURLConnection) url.openConnection();
             http.setRequestMethod("PUT");
             http.setDoOutput(false);
@@ -173,7 +173,7 @@ public class Algo_FahrerAuswahl {
 
         try
         {
-            URL url = new URL("http:/eat4speed.xyz/#/Auftrag/getAuftragFahrernummerByAuftrags_ID/" + id);
+            URL url = new URL("https://eat4speed.xyz/Auftrag/getAuftragFahrernummerByAuftrags_ID/" + id);
             HttpURLConnection http = (HttpURLConnection) url.openConnection();
             http.setRequestMethod("GET");
             http.setDoOutput(true);
@@ -197,7 +197,7 @@ public class Algo_FahrerAuswahl {
 
         try
         {
-            URL url = new URL("http:/eat4speed.xyz/#/Benachrichtigung_Fahrer/");
+            URL url = new URL("https://eat4speed.xyz/#/Benachrichtigung_Fahrer/");
             HttpURLConnection http = (HttpURLConnection) url.openConnection();
             http.setRequestMethod("POST");
             http.setDoOutput(true);
@@ -225,7 +225,7 @@ public class Algo_FahrerAuswahl {
 
         try
         {
-            URL url = new URL("http:/eat4speed.xyz/#/BenachrichtigungFahrerAuftrag/");
+            URL url = new URL("https://eat4speed.xyz/BenachrichtigungFahrerAuftrag/");
             HttpURLConnection http = (HttpURLConnection) url.openConnection();
             http.setRequestMethod("PUT");
             http.setDoOutput(true);
@@ -253,7 +253,7 @@ public class Algo_FahrerAuswahl {
 
         try
         {
-            URL url = new URL("http:/eat4speed.xyz/#/Benachrichtigung_Fahrer/id");
+            URL url = new URL("https://eat4speed.xyz/Benachrichtigung_Fahrer/id");
             HttpURLConnection http = (HttpURLConnection) url.openConnection();
             http.setRequestMethod("GET");
             http.setDoOutput(true);
@@ -298,7 +298,7 @@ public class Algo_FahrerAuswahl {
         {
             try
             {
-                URL url = new URL("http:/eat4speed.xyz/#/BenachrichtigungFahrerAuftrag/" + auftragID + "/" + i);
+                URL url = new URL("https://eat4speed.xyz/BenachrichtigungFahrerAuftrag/" + auftragID + "/" + i);
                 HttpURLConnection http = (HttpURLConnection) url.openConnection();
                 http.setRequestMethod("DELETE");
                 http.setDoOutput(false);
@@ -311,7 +311,7 @@ public class Algo_FahrerAuswahl {
 
             try
             {
-                URL url = new URL("http:/eat4speed.xyz/#/Benachrichtigung_Fahrer/" + i);
+                URL url = new URL("https://eat4speed.xyz/Benachrichtigung_Fahrer/" + i);
                 HttpURLConnection http = (HttpURLConnection) url.openConnection();
                 http.setRequestMethod("DELETE");
                 http.setDoOutput(false);
@@ -394,7 +394,7 @@ public class Algo_FahrerAuswahl {
 
         try
         {
-            URL url = new URL("http:/eat4speed.xyz/#/Urlaub/HeuteUrlaub/" + fahrernummer);
+            URL url = new URL("https://eat4speed.xyz/Urlaub/HeuteUrlaub/" + fahrernummer);
             HttpURLConnection http = (HttpURLConnection) url.openConnection();
             http.setRequestMethod("GET");
             http.setDoOutput(true);
